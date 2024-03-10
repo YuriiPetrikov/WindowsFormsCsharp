5d06d3f470056ec70b4e9080f64bd210726d2bf3
Юрий Петриков <146861042+YuriiPetrikov@users.noreply.github.com>
2024-03-10 17:24:29 +0700

    Add files via upload

---
+++ TicTacToe/Form1.Designer.cs
@@ -0,0 +1,116 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnNewGame = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // btnNewGame
+            // 
+            this.btnNewGame.Location = new System.Drawing.Point(351, 17);
+            this.btnNewGame.Name = "btnNewGame";
+            this.btnNewGame.Size = new System.Drawing.Size(75, 23);
+            this.btnNewGame.TabIndex = 9;
+            this.btnNewGame.TabStop = false;
+            this.btnNewGame.Tag = "10";
+            this.btnNewGame.Text = "Новая игра";
+            this.btnNewGame.UseVisualStyleBackColor = true;
+            this.btnNewGame.Click += new System.EventHandler(this.btnNewGame_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(348, 63);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 13);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Текущий ход:";
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(300, 300);
+            this.panel1.TabIndex = 11;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(351, 99);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(132, 17);
+            this.radioButton1.TabIndex = 13;
+            this.radioButton1.Text = "Игрок против игрока";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(351, 122);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(160, 17);
+            this.radioButton2.TabIndex = 14;
+            this.radioButton2.Text = "Игрок против компьютера";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoValidate = System.Windows.Forms.AutoValidate.Disable;
+            this.ClientSize = new System.Drawing.Size(511, 328);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnNewGame);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "TicTacToe";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnNewGame;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+    }
+}
+
---
+++ TicTacToe/TicTacToeModel.cs
@@ -0,0 +1,206 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    internal class TicTacToeModel
+    {
+        private readonly int?[] map;
+        private readonly int size;
+        private readonly Random rnd;
+        public delegate int StrategyDelegate();
+
+        public TicTacToeModel(int size)
+        {
+            this.size = size;
+            map = new int?[size * size];
+            rnd = new Random();
+        }
+        public int? this[int index]
+        {
+            get => map[index];
+            set { map[index] = value; }
+        }
+        public bool IsCheck()
+        {
+            int sum;
+            //проверка rows
+            for (int i = 0; i < size * size; i += size)
+            {
+                if (map[i] == null)
+                    continue;
+                sum = 0;
+                for (int j = 0; j < size; j++)
+                    if (map[i].Equals(map[i + j]))
+                        sum++;
+                if (sum == size)
+                    return true;
+            }
+            //проверка collums
+            for (int i = 0; i < size; i++)
+            {
+                if (map[i] == null)
+                    continue;
+                sum = 0;
+                for (int j = 0; j < size; j++)
+                    if (map[i].Equals(map[i + size * j]))
+                        sum++;
+                if (sum == size)
+                    return true;
+            }
+            //проверка diagonal_1
+            sum = 0;
+            if (map[0] != null)
+            {
+                for (int i = 0; i < size; i++)
+                    if (map[0].Equals(map[i + size * i]))
+                        sum++;
+                if (sum == size)
+                    return true;
+            }
+            //проверка diagonal_2
+            if (map[size - 1] != null)
+            {
+                sum = 0;
+                for (int i = 1; i <= size; i++)
+                    if (map[size - 1].Equals(map[size * i - i]))
+                        sum++;
+                if (sum == size)
+                    return true;
+            }
+            return false;
+        }
+        public void NewInitianal()
+        {
+            for (int i = 0; i < size * size; i++)
+                map[i] = null;
+        }
+        public bool IsStandoff()
+        {
+            foreach (int? i in map)
+                if (i == null)
+                    return false;
+            return true;
+        }
+        public int RundomPositionComputer(StrategyDelegate strategyDelegate)
+        {
+            return strategyDelegate() == -1 ? EasyStrategyAttackOrProtection() : strategyDelegate();
+        }
+        public int EasyStrategyAttackOrProtection()
+        {
+            int position;
+            while (true)
+            {
+                position = rnd.Next(size * size);
+                for (int i = 0; i < size * size; i++)
+                    if (map[i] == null && --position == 0)
+                        return i;
+            }
+        }
+        public int AverageStrategyAttackOrProtection()
+        {
+            int sum;
+            int pos = -1;
+            int[] move = {1, 0};
+            for(int k = 0; k < move.Length; k++)
+            {
+                //проверка rows
+                for (int i = 0; i < size * size; i += size)
+                {
+                    sum = 0;
+
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (map[i + j] == null)
+                            pos = i + j;
+
+                        if (map[i + j] == move[k])
+                            sum++;
+                    }
+
+                    if (sum == size - 1 && pos != -1)
+                        return pos;
+
+                    pos = -1;
+                }
+            }
+        
+            return pos;
+        }
+        public int DifficultStrategyAttackOrProtection()
+        {
+            if (AverageStrategyAttackOrProtection() != -1)
+                return AverageStrategyAttackOrProtection();
+
+            int sum;
+            int pos = -1;
+            int[] move = { 1, 0 };
+
+            for (int k = 0; k < move.Length; k++)
+            {
+                //проверка collums
+                for (int i = 0; i < size; i++)
+                {
+                    sum = 0;
+                    for (int j = 0; j < size; j++)
+                    {
+                        if (map[i + size * j] == null)
+                            pos = i + size * j;
+                        if (map[i + size * j] == move[k])
+                            sum++;
+                    }
+
+                    if (sum == size - 1 && pos != -1)
+                    {
+                        return pos;
+                    }
+                        
+
+                    pos = -1;
+                }
+            }
+    
+            return pos;
+        }
+        public int SuperDifficultStrategyAttackOrProtection()
+        {
+            if (DifficultStrategyAttackOrProtection() != -1)
+                return DifficultStrategyAttackOrProtection();
+            
+            int[] move = { 1, 0 };
+            int sum = 0;
+            int pos = -1;
+            //проверка diagonal_1
+            for (int k = 0; k < move.Length; k++)
+            {
+                for (int i = 0; i < size; i++)
+                {
+                    if (map[i + size * i] == null)
+                        pos = i + size * i;
+                    if (map[i + size * i] == move[k])
+                        sum++;
+                }
+
+                if (sum == size - 1 && pos != -1)
+                    return pos;
+            }
+
+            //проверка diagonal_2
+            sum = 0;
+            pos = -1;
+            for (int k = 0; k < move.Length; k++)
+            {
+                for (int i = 1; i <= size; i++)
+                {
+                    if (map[size * i - i] == null)
+                        pos = size * i - i;
+                    if (map[size * i - i] == move[k])
+                        sum++;
+                }
+                if (sum == size - 1 && pos != -1)
+                    return pos;
+            }
+        
+            return -1;
+        }
+    }
+}
