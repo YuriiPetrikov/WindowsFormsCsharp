63f6893b1b8bc0191669da2b147c8212e8c49948
Юрий Петриков <146861042+YuriiPetrikov@users.noreply.github.com>
2024-04-30 11:44:04 +0700

    Add files via upload

---
+++ Игра в 15 (пятнашки) v.2/RoundedButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace Игра_в_15__пятнашки_
+{
+    internal class RoundedButton : Button
+    {
+        [DllImport("Gdi32.dll", EntryPoint = "CreateRoundRectRgn")]
+        private static extern IntPtr RoundButton(int nLeftRect, int nTopRect, int nRightRect, int nBottomRect, int nWidthEllipse, int nHeightEllipse);
+
+        protected override void OnPaint(PaintEventArgs pevent)
+        {
+            base.OnPaint(pevent);
+            IntPtr ptr = RoundButton(0, 0, Width, Height, 20, 20);
+            Region = Region.FromHrgn(ptr);
+        }
+    }
+}
